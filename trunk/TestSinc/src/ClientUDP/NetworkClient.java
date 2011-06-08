cf240a7f246e4ef7a495ef24a30e6a003d84ea8d
mombelli.mauro@gmail.com <mombelli.mauro@gmail.com@f4f5d84e-4b61-536b-215a-389b5e49ec10>
2011-06-08 00:08:03 +0000

    New UDP implementation

---
+++ trunk/TestSinc/src/ClientUDP/NetworkClient.java
@@ -0,0 +1,31 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ClientUDP;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.DatagramChannel;
+
+/**
+ *
+ * @author mauro
+ */
+class NetworkClient{
+    DatagramChannel client;
+
+    NetworkClient(String ip, int porta) throws IOException{
+        client = DatagramChannel.open();
+        client.configureBlocking(false);
+        client.connect(new InetSocketAddress(ip, porta));
+    }
+
+    void write(ByteBuffer c) throws IOException {
+        c.flip();
+        System.out.println("scritti:"+ client.write(c)+" byte");
+    }
+
+}
---
+++ trunk/TestSinc/src/ServerUDP/MyDatagram.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ServerUDP;
+
+import java.net.SocketAddress;
+import java.nio.ByteBuffer;
+
+/**
+ *
+ * @author mauro
+ */
+class MyDatagram {
+    SocketAddress address;
+    ByteBuffer data;
+}
