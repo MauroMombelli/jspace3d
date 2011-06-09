b6fa36ca1a4832c9e95dcbdf770452fa150903cf
goffredo.goffrei@gmail.com <goffredo.goffrei@gmail.com@f4f5d84e-4b61-536b-215a-389b5e49ec10>
2011-06-09 17:25:36 +0000

    Separated graphics in another project.

---
+++ trunk/GraphicTest/src/game/GameClientEntity.java
@@ -0,0 +1,53 @@
+package game;
+
+import javax.vecmath.Quat4f;
+
+import com.ardor3d.math.Quaternion;
+import com.ardor3d.scenegraph.Spatial;
+import com.bulletphysics.dynamics.RigidBody;
+import com.bulletphysics.linearmath.Transform;
+import game.physics.utils.ArdorMotionState;
+
+/**
+ * Extends {@link GameEntity}. <br>
+ * <p>
+ * Will provide specific functions to the client's entities, in particular to
+ * the physical simulation and graphical update pipeline.
+ * </p>
+ * <br>
+ * 
+ * @author Goffredo Goffrei
+ * 
+ */
+
+public class GameClientEntity extends GameEntity {
+
+	protected Quaternion quat;
+	protected Transform xForm;
+
+	protected GameClientEntity() {
+		quat = new Quaternion();
+		xForm = new Transform();
+	}
+
+	/**
+	 * @param physicalEntity
+	 * @param graphicalEntity
+	 */
+	public GameClientEntity(RigidBody physicalEntity, Spatial graphicalEntity) {
+		super(physicalEntity, graphicalEntity);
+		quat = new Quaternion();
+		xForm = new Transform();
+	}
+
+	public void syncGraphicsWithPhysics() {
+		xForm = ((ArdorMotionState) getPhysicalEntity().getMotionState()).graphicsWorldTrans;
+		getGraphicalEntity().setTranslation(xForm.origin.x, xForm.origin.y,
+				xForm.origin.z);
+		quat.set(xForm.getRotation(new Quat4f()).x,
+				xForm.getRotation(new Quat4f()).y,
+				xForm.getRotation(new Quat4f()).z,
+				xForm.getRotation(new Quat4f()).w);
+		getGraphicalEntity().setRotation(quat);
+	}
+}
