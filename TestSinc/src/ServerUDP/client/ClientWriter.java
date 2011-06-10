5d7923b26aa30f6d8d5524ac728daf8efda1ae41
mombelli.mauro@gmail.com <mombelli.mauro@gmail.com@f4f5d84e-4b61-536b-215a-389b5e49ec10>
2011-06-10 01:52:25 +0000

    Update game structure

--- TestSinc/src/ServerUDP/client/ClientWriter.java
+++ TestSinc/src/ServerUDP/client/ClientWriter.java
@@ -2,11 +2,12 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package ServerUDP.client;
 
 import java.io.IOException;
+import java.net.InetAddress;
 import java.net.InetSocketAddress;
+import java.net.NetworkInterface;
 import java.net.SocketAddress;
 import java.nio.ByteBuffer;
 import java.nio.channels.DatagramChannel;
@@ -19,27 +20,44 @@ import java.util.logging.Logger;
  */
 public class ClientWriter {
     private DatagramChannel outputChannel;
+    private ByteBuffer output;
 
-    ClientWriter(SocketAddress address) {
+    public ClientWriter(SocketAddress address) {
+
         try {
             outputChannel = DatagramChannel.open();
             outputChannel.configureBlocking(false);
             InetSocketAddress adr = (InetSocketAddress) address;
             adr = new InetSocketAddress(adr.getHostName(), 5001);
             outputChannel.connect(adr);
+
+            NetworkInterface network = NetworkInterface.getByInetAddress( ((InetSocketAddress)outputChannel.socket().getLocalSocketAddress()).getAddress() );
+            int MTU_MINUS_UDP_HEADER = network.getMTU()-100;
+            System.out.println(network.getDisplayName()+" rilevated MTU: "+network.getMTU());
+
+            output = ByteBuffer.allocateDirect(MTU_MINUS_UDP_HEADER);
         } catch (IOException ex) {
             Logger.getLogger(ClientWriter.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 
-    void write(ByteBuffer t) throws IOException {
-        //t.flip();
-        int byteWritten = outputChannel.write(t);
-        //System.out.println( "Writed:"+byteWritten+" byte "+t.capacity()+" "+t.limit() );
-        
-        if (byteWritten!=t.limit()){
-            System.out.println( "Written only:"+byteWritten+" byte of "+t.limit() );
+    public void write(ByteBuffer byteToWrite) throws IOException {
+        if (output.limit() + byteToWrite.limit() > output.capacity()) {
+            //datagram is full, send it
+            flush();
         }
-     
+        output.put(byteToWrite);
+    }
+
+    public void flush() throws IOException {
+        output.flip();
+        int byteWritten = outputChannel.write(output);
+
+        if (byteWritten != output.limit()) {
+            System.out.println("Written only:" + byteWritten + " byte of " + output.limit());
+            output.compact();
+        } else {
+            output.clear();
+        }
     }
 }
