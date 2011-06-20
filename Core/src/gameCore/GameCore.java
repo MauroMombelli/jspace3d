2330c4c2648cd37d6dbe23324ab497ec026fe2b7
goffredo.goffrei@gmail.com <goffredo.goffrei@gmail.com@f4f5d84e-4b61-536b-215a-389b5e49ec10>
2011-06-20 23:28:00 +0000

    Starting all over.

---
+++ Core/src/gameCore/GameCore.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package gameCore;
+
+import com.ardor3d.framework.Updater;
+import com.ardor3d.util.ReadOnlyTimer;
+
+/**
+ *
+ * @author Fra
+ */
+public class GameCore implements Updater{
+
+    public void init() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void update(ReadOnlyTimer timer) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+}
